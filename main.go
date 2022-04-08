c95a8757da3f76eaad2372c3856343acc651a376
mxgnus-de <magnus.bache@outlook.de>
2022-04-08 21:35:45 +0200

    added custom upload dir

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/gofiber/fiber/v2"
@@ -14,6 +15,7 @@ func main() {
 	envHost := os.Getenv("HOST_NAME")
 	envScheme := os.Getenv("HOST_SCHEME")
 	envPort := os.Getenv("PORT")
+	uploadDir := os.Getenv("UPLOAD_DIR")
 
 	if envKey == "" {
 		panic("UPLOAD_KEY is not set. Please set it in .env file")
@@ -31,6 +33,19 @@ func main() {
 		panic("HOST_SCHEME is not set. Please set it in .env file")
 	}
 
+	if uploadDir == "" {
+		uploadDir = "./uploads"
+		fmt.Println("UPLOAD_DIR is not set. Using default:", uploadDir)
+	}
+
+	if _, err := os.Stat(uploadDir); os.IsNotExist(err) {
+		fmt.Println("Upload directory does not exist. Creating... " + uploadDir)
+		err = os.MkdirAll(uploadDir, 0755)
+		if err != nil {
+			panic(err)
+		}
+	}
+
 	_, err := os.Stat("uploads")
 
 	if err != nil {
@@ -45,7 +60,7 @@ func main() {
 	api := app.Group("/api")
 
 	app.Static("/", "react-app/build")
-	app.Static("/", "uploads")
+	app.Static("/", uploadDir)
 
 	api.Post("/upload", func(c *fiber.Ctx) error {
 		file, err := c.FormFile("file")
@@ -69,7 +84,7 @@ func main() {
 			})
 		}
 
-		err = c.SaveFile(file, "./uploads/"+file.Filename)
+		err = c.SaveFile(file, uploadDir+"/"+file.Filename)
 
 		if err != nil {
 			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
